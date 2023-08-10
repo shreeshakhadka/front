bc917b0f4f796631c5954fc8bfff421c979aeb22
shreeshakhadka <khadkashreesha08@gmail.com>
2023-08-10 16:39:22 -0400

    #

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,38 @@
 import './App.css'
+import CardComponent from './components/Card.comp'
 
 function App() {
 
   return (
-    <>
+    
+  <div style={{display:"flex", gap:"2rem"}} >
+
+  <CardComponent 
+    user={"SK"}
+    name ={"Fishtail Mountain"} 
+    image ={"https://www.himalayanwonders.com/siteblog/wp-content/uploads/2016/01/fishtail-mountain.jpg"}
+    descrption="Fishtail Mountain, also known as Machapuchare, is a stunning peak located in the
+     Annapurna Himalayas of Nepal. Its distinctive fishtail-like double summit makes it a prominent 
+     landmark in the region. Revered by both trekkers and climbers, Fishtail Mountain's beauty and 
+     challenging terrain contribute to its allure in the heart of Nepal's mountainous landscape."
+    />
+
+<CardComponent 
+    user={"KS"}
+    name ={"Annapurna Mountain"} 
+    image ={"https://upload.wikimedia.org/wikipedia/commons/f/fa/Annapurna_South-4588.jpg"}
+    descrption="Annapurna Mountain, part of the Annapurna Himalayan range in Nepal, is one
+     of the world's highest peaks, standing at 8,091 meters (26,545 feet). Renowned for its 
+     breathtaking beauty and challenging climbs, Annapurna is a coveted destination for 
+     trekkers and mountaineers, offering a remarkable experience amid the awe-inspiring
+     landscapes of the Himalayas."
+    />
       
      
-    </>
-  )
+    </div>
+  ) 
+  
 }
+
 
 export default App
--- src/components/Card.comp.tsx
+++ src/components/Card.comp.tsx
@@ -2,13 +2,20 @@ import { Avatar, Card, CardActions, CardContent, CardHeader, CardMedia, IconButt
 import { red } from "@mui/material/colors"
 import {MoreVert, Favorite } from '@mui/icons-material'
 
-const CardComponent : React.FC<unknown> =() => {
+
+interface Props{
+    image: string;
+    name: string;
+    user: string;
+    descrption : string;
+}
+const CardComponent : React.FC<Props> =({image,name, user, descrption}) => {
     return (
         <Card sx={{ maxWidth: 345 }}>
           <CardHeader
             avatar={
               <Avatar sx={{ bgcolor: red[500] }} aria-label="recipe">
-                R
+                {user}
               </Avatar>
             }
             action={
@@ -16,20 +23,18 @@ const CardComponent : React.FC<unknown> =() => {
                 <MoreVert />
               </IconButton>
             }
-            title="Shrimp and Chorizo Paella"
-            subheader="September 14, 2016"
+            title={name}
+            subheader={new Date(). toLocaleDateString()}
           />
           <CardMedia
             component="img"
             height="194"
-            image="/static/images/cards/paella.jpg"
+            image={image}
             alt="Paella dish"
           />
           <CardContent>
             <Typography variant="body2" color="text.secondary">
-              This impressive paella is a perfect party dish and a fun meal to cook
-              together with your guests. Add 1 cup of frozen peas along with the mussels,
-              if you like.
+              {descrption}
             </Typography>
           </CardContent>
           <CardActions disableSpacing>
