01f71be81a3bcd520d680d8306be048ca0d5212b
shreeshakhadka <khadkashreesha08@gmail.com>
2023-08-10 14:18:10 -0400

    Initial commit

---
+++ src/components/Card.comp.tsx
@@ -0,0 +1,48 @@
+import { Avatar, Card, CardActions, CardContent, CardHeader, CardMedia, IconButton, Typography } from "@mui/material"
+import { red } from "@mui/material/colors"
+import {MoreVert, Favorite } from '@mui/icons-material'
+
+const CardComponent : React.FC<unknown> =() => {
+    return (
+        <Card sx={{ maxWidth: 345 }}>
+          <CardHeader
+            avatar={
+              <Avatar sx={{ bgcolor: red[500] }} aria-label="recipe">
+                R
+              </Avatar>
+            }
+            action={
+              <IconButton aria-label="settings">
+                <MoreVert />
+              </IconButton>
+            }
+            title="Shrimp and Chorizo Paella"
+            subheader="September 14, 2016"
+          />
+          <CardMedia
+            component="img"
+            height="194"
+            image="/static/images/cards/paella.jpg"
+            alt="Paella dish"
+          />
+          <CardContent>
+            <Typography variant="body2" color="text.secondary">
+              This impressive paella is a perfect party dish and a fun meal to cook
+              together with your guests. Add 1 cup of frozen peas along with the mussels,
+              if you like.
+            </Typography>
+          </CardContent>
+          <CardActions disableSpacing>
+            <IconButton aria-label="add to favorites">
+              <Favorite />
+            </IconButton>
+            <IconButton aria-label="share">
+              <Favorite />
+            </IconButton>
+            
+          </CardActions>
+       
+        </Card>
+      );
+}
+export default CardComponent;
